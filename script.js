f7aadb00d03a8db6f6ce003cf4cdc8217fd78977
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2025-01-31 16:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function submitForm() {
+ function submitForm() {
             // Get the values from input fields
-            const firstName = document.getElementById("firstName").value;
-            const lastName = document.getElementById("lastName").value;
-            const phoneNumber = document.getElementById("phoneNumber").value;
-            const email = document.getElementById("email").value;
+            const firstName = document.getElementById("firstName").value.trim();
+            const lastName = document.getElementById("lastName").value.trim();
+            const phoneNumber = document.getElementById("phoneNumber").value.trim();
+            const email = document.getElementById("email").value.trim();
 
             // Format the alert message
             const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`;
